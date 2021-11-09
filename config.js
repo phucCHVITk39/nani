f25f00eb8fcb24d4fe18aa59a8cf530c5e429ad2
phucCHVITk39 <93048727+phucCHVITk39@users.noreply.github.com>
2021-11-09 20:17:04 +0700

    234

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "bí mật",
+    introTitle: 'Cbạn yêu dấu',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng anh đã yêu em ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Thoát web là chấp nhận yêu tớ rồi nhé!!!',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Tối nay, mình qua đón đi chơi nha.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.messenger.com/t/100036274192039' 
+}
